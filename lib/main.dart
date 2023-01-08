3409a3af2b89ba75e95f0978451dec05cb075963
Ahsan Ali <60280488+Phant0m-a@users.noreply.github.com>
2023-01-08 13:42:14 +0500

    Add files via upload
    - this code needs a lot of refactoring!!!

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,26 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
+  void initState() {
+    // checks if user is currently signed-in
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+    super.initState();
+  }
+
+  //Events are fired when the following occurs:
+  //
+  // Right after the listener has been registered.
+  // When a user is signed in.
+  // When the current user is signed out.
+  // When there is a change in the current user's token.
+
+  @override
   Widget build(BuildContext context) {
     // return const Auth();
     return MaterialApp(
@@ -32,7 +52,8 @@ class _MyAppState extends State<MyApp> {
           elevation: 0,
           backgroundColor: Colors.white,
           leading: Container(
-            padding: const EdgeInsets.only(left: 10, top: 0, right: 5, bottom: 0),
+            padding:
+                const EdgeInsets.only(left: 10, top: 0, right: 5, bottom: 0),
             child: CircleAvatar(
               backgroundColor: Colors.grey[400],
               radius: 20,
@@ -78,62 +99,197 @@ class _MyAppState extends State<MyApp> {
               ),
 
               Container(
-                //************** i'll change it to carousal slider soon 😊
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  border: Border.all(color: Colors.black),
-                ),
-                height: 180,
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                child: Image.network(
-                  //'https://docs.flutter.dev/assets/images/docs/development/ui/splash-screen/android-splash-screen/splash-screens_header.png',
-                  'lib/assets/slider.png',
-                  fit: BoxFit.fill,
-                ),
-              ),
+                  //************** i'll change it to carousal slider soon 😊
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(color: Colors.black),
+                  ),
+                  height: 180,
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                  child:
+                      // Image.network(
+                      //   'https://docs.flutter.dev/assets/images/docs/development/ui/splash-screen/android-splash-screen/splash-screens_header.png',
+                      //   //
+                      //   fit: BoxFit.fill,
+                      // ),
+                      Image.asset(
+                    'lib/assets/slider.PNG',
+                    fit: BoxFit.fill,
+                  )),
 
-              Container( padding: const EdgeInsets.only(left: 15, top: 20, right: 5, bottom: 0),
+              Container(
+                padding: const EdgeInsets.only(
+                    left: 15, top: 20, right: 5, bottom: 0),
                 child: Row(
-                  children:  [
-
-
-                      const CircleAvatar(
-                        backgroundColor: Colors.redAccent,
-                        radius: 30,
-                        child: Icon(
-                          Icons.category_rounded,
-                          color: Colors.white,
-                          size: 35,
+                  children: [
+                    Column(
+                      children: [
+                        const CircleAvatar(
+                          backgroundColor: Colors.redAccent,
+                          radius: 30,
+                          child: Icon(
+                            Icons.category_rounded,
+                            color: Colors.white,
+                            size: 35,
+                          ),
                         ),
-                      ),
-                    Container(
-                      padding: const EdgeInsets.only(left: 10, top: 0, right: 5, bottom: 0),
-                      child: CircleAvatar(
-                        backgroundColor: Colors.grey[400],
-                        radius: 30,
-                        child: Icon(
-                          Icons.image,
-                          color: Colors.grey[100],
-                          size: 35,
-                        ),
-                      ),
+                        SizedBox(height: 20),
+                        Text('Categories')
+                      ],
                     ),
-                    Container(
-                      padding: const EdgeInsets.only(left: 10, top: 0, right: 5, bottom: 0),
-                      child: CircleAvatar(
-                        backgroundColor: Colors.grey[400],
-                        radius: 30,
-                        child: Icon(
-                          Icons.image,
-                          color: Colors.grey[100],
-                          size: 35,
+                    Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.only(
+                              left: 10, top: 0, right: 5, bottom: 0),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.grey[400],
+                            radius: 30,
+                            child: Icon(
+                              Icons.image,
+                              color: Colors.grey[100],
+                              size: 35,
+                            ),
+                          ),
                         ),
-                      ),
+                        SizedBox(height: 20),
+                        Text('Men')
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.only(
+                              left: 10, top: 0, right: 5, bottom: 0),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.grey[400],
+                            radius: 30,
+                            child: Icon(
+                              Icons.image,
+                              color: Colors.grey[100],
+                              size: 35,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text('Women')
+                      ],
                     ),
                   ],
                 ),
-              )
+              ),
+
+              Divider(
+                thickness: 0.5,
+                color: Colors.grey,
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: 15.0, top: 5, bottom: 5, right: 15),
+                    child: Text(
+                      'Products for you',
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.redAccent),
+                    ),
+                  ),
+                ],
+              ),
+              Divider(
+                thickness: 0.5,
+                color: Colors.grey,
+              ),
+
+              // Grid
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                child: Container(
+                    decoration: BoxDecoration(),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'lib/assets/slider.PNG',
+                          fit: BoxFit.fill,
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              Text('Casual Men Shirt'),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Rs. 850',
+                                    style: TextStyle(
+                                      color: Colors.red[100],
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  Icon(
+                                    Icons.share_rounded,
+                                    color: Colors.redAccent,
+                                    size: 26,
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 8, vertical: 4),
+                                    decoration: BoxDecoration(
+                                        color: Colors.pink[100],
+                                        borderRadius:
+                                            BorderRadius.circular(12)),
+                                    child: Text(
+                                      'Free Delivery',
+                                      style: TextStyle(
+                                          color: Colors.grey, fontSize: 12),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 7,
+                                  ),
+                                  Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.baseline,
+                                    textBaseline: TextBaseline.alphabetic,
+                                    children: [
+                                      Text(
+                                        '100',
+                                        style: TextStyle(
+                                            color: Colors.red[100],
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16),
+                                      )
+                                      // ,SizedBox(width: 2,),
+                                      ,
+                                      Text(
+                                        'km',
+                                        style: TextStyle(
+                                            color: Colors.grey, fontSize: 10),
+                                      )
+                                    ],
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    )),
+              ),
             ],
           )),
         ),
